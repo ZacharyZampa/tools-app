336f0f69e96f94834160d3c359c49a683b2da5c5
Zachary Zampa <zackzampa@gmail.com>
2021-03-03 09:16:48 -0500

    remove fake gpa data

--- src/app/gpa-calculator/course.service.ts
+++ src/app/gpa-calculator/course.service.ts
@@ -13,6 +13,8 @@ export class CourseService {
   constructor(private http: HttpClient) {}
 
   getCourseList(): Course[] {
+
+    // --- Use this if I ever feel / get around to making user auth and saving of courses
     // this.http.get<{message: string, posts: Course[]}>('http://localhost:3000/api/v1/courses')
     //   .subscribe((courseData) => {
     //     this.courseList = courseData.posts;
@@ -20,8 +22,16 @@ export class CourseService {
     //   });
 
     this.courseList = new Array();
-    for (let i = 0; i < 20; i++) {
-      this.courseList.push(new Course("" + i, "CSE " + i, 3.7, 3));
+
+    // --- Use this for sample fake data
+    // for (let i = 0; i < 20; i++) {
+    //   this.courseList.push(new Course("" + i, "CSE " + i, 3.7, 3));
+    //   this.coursesUpdated.next(this.courseList.slice());
+    // }
+
+    // sample course if none added yet
+    if (this.courseList.length === 0) {
+      this.courseList.push(new Course("1", "CSE " + 101, 3.7, 3));
       this.coursesUpdated.next(this.courseList.slice());
     }
     
